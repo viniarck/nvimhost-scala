57289b4ac848048853670e9d84590f87a82f476a
Vinicius Arcanjo <viniarck@gmail.com>
2019-05-15 20:14:52 -0300

    Refactored Plugin.scala as an abstract class and support for async methods

--- src/main/scala/nvimhost/Plugin.scala
+++ src/main/scala/nvimhost/Plugin.scala
@@ -13,19 +13,58 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Try, Success, Failure}
 import collection.mutable.ArrayBuffer
 
+/** Abstraction that represents a Nvim Plugin */
+abstract class Plugin(
+    address: InetSocketAddress,
+    packageObjectName: String,
+    outVimFilePath: String,
+    jarFilePath: String
+) extends LazyLogging {
+
+  logger.info("Validating plugin name and methods")
+  try {
+    Reflection.validatePluginStructure(packageObjectName)
+  } catch {
+    case e: Exception => logger.error(e.toString()); System.exit(1)
+  }
+
+  logger.info(s"Generating manifest vim file: $outVimFilePath for: $packageObjectName, executable jar: $jarFilePath")
+  Reflection.genPluginFile(
+    packageObjectName,
+    outVimFilePath,
+    jarFilePath
+  )
+
+  val nvim = new NvimAPI(
+    new InetSocketAddress(address.getHostName, address.getPort)
+  )
+  val jarName = jarFilePath.split(java.io.File.separator).last.replace(".jar", "")
+  val system = ActorSystem(jarName)
+  val pluginActor = system.actorOf(
+    (Props(new PluginActor(address, system, packageObjectName, nvim, jarName)))
+  )
+
+  def serveForever() = {
+    while (true) {
+      Thread.sleep(60000)
+    }
+  }
+
+  override def toString() = s"Plugin($jarName)"
+}
+
 /** Actor to represent an IO/TCP plugin, it's channel is dedicated to receive and reply RPC message. This actor needs to embed an instance of NvimAPI since the plugin has to set a variable to bootstrap the plugin RPC channel and send messages to Nvim */
 class PluginActor(
     address: InetSocketAddress,
     actorSystem: ActorSystem,
+    packageObjectName: String,
+    nvim: NvimAPI,
     jarName: String
 ) extends Actor
     with LazyLogging {
 
   logger.info("Bootstraping plugin socket")
-  val jarNameFile = jarName.replace(".jar", "")
-  val client = new NvimAPI(
-    new InetSocketAddress(address.getHostName, address.getPort)
-  )
+  val jarNameFile = jarName
 
   IO(Tcp)(actorSystem) ! Connect(address)
   var channelId: Int = 0
@@ -50,7 +89,7 @@ class PluginActor(
           logger.debug(
             s"Received arr: ${decoded.arr} size: ${decoded.arr.length}"
           )
-          if (decoded.arr.length != 4) {
+          if (decoded.arr.length < 2 || decoded.arr.length > 4) {
             logger.error(
               "Nvim RPC response format has changed. Please file an issue on https://github.com/viniarck/nvimhost-scala"
             )
@@ -63,7 +102,7 @@ class PluginActor(
               channelId = decoded.arr(3)(0).num.toInt
               logger.debug(s"Trying to set channelId as '${channelId}'")
               val fut =
-                client.command(s"let g:${jarNameFile}_channel=${channelId}")
+                nvim.command(s"let g:${jarNameFile}_channel=${channelId}")
               Try(Await.result(fut, 1.second)) match {
                 case Success(v) =>
                   logger.info(
@@ -76,12 +115,26 @@ class PluginActor(
               case e: Exception => logger.error(e.toString())
             }
           } else {
-            // TODO method not found..
-            // TODO display err on Vim
-            val msgId: Int = decoded.arr(1).value.asInstanceOf[Double].toInt
-            val methodName: String = decoded.arr(2).value.toString()
+            val msgType: Int = decoded.arr(0).value.asInstanceOf[Double].toInt
+            var msgId: Int = 0
+            var methodName: String = ""
+            var argsIndex = 0
+            msgType match {
+              case 0 => {
+                logger.debug("Received a sync request from nvim")
+                msgId = decoded.arr(1).value.asInstanceOf[Double].toInt
+                methodName = decoded.arr(2).value.toString()
+                argsIndex = 3
+              }
+              case 2 => {
+                logger.debug("Received an async request from nvim")
+                methodName = decoded.arr(1).value.toString()
+                argsIndex = 2
+              }
+              case _ => logger.error(s"Received wrong message type: ${msgType}")
+            }
             val argsIn: ArrayBuffer[Any] =
-              decoded.arr(3).value.asInstanceOf[ArrayBuffer[Any]]
+              decoded.arr(argsIndex).value.asInstanceOf[ArrayBuffer[Any]]
             logger.debug(
               s"Response data: ${methodName} ${argsIn.getClass().toString}"
             )
@@ -91,76 +144,76 @@ class PluginActor(
             )
             try {
               val res = Reflection.call(
-                "nvimhost.Foo$",
+                packageObjectName,
                 methodName.split(":").last,
                 args.value.toArray: _*
               )
-              // val res = Reflection.call("nvimhost.Foo$", "Heck", ujson.Num(10))
               logger.debug(
                 s"Response res: ${res} res class: ${res.getClass.toString}"
               )
-              val response: ResponseMsg = res match {
-                case s: ujson.Str =>
-                  new ResponseMsg(
-                    msgId,
-                    upack.Null,
-                    upack.Str(s.str)
-                  )
-                case i: ujson.Num => {
-                  if (math.ceil(i.num) == i.num)
+              if (msgType == 0) {
+                val response: ResponseMsg = res match {
+                  case s: ujson.Str =>
                     new ResponseMsg(
                       msgId,
                       upack.Null,
-                      upack.Int32(i.num.toInt)
+                      upack.Str(s.str)
                     )
-                  else
-                    new ResponseMsg(
-                      msgId,
-                      upack.Null,
-                      upack.Float64(i.num)
-                    )
-                }
-                case b: ujson.Bool =>
-                  new ResponseMsg(
-                    msgId,
-                    upack.Null,
-                    upack.Bool(b.value)
-                  )
-                case a: ujson.Arr => {
-                  import Conversions._
-                  var args = ArrayBuffer[upack.Msg]()
-                  for (item <- a.arr.map(_.value).toArray) {
-                    if (item.isInstanceOf[Double])
-                      args += (item.asInstanceOf[Double])
-                    else if (item.isInstanceOf[Int])
-                      args += (item.asInstanceOf[Int])
-                    else if (item.isInstanceOf[Boolean])
-                      args += (item.asInstanceOf[Boolean])
-                    else if (item.isInstanceOf[String])
-                      args += (item.toString)
+                  case i: ujson.Num => {
+                    if (math.ceil(i.num) == i.num)
+                      new ResponseMsg(
+                        msgId,
+                        upack.Null,
+                        upack.Int32(i.num.toInt)
+                      )
                     else
-                      throw new RuntimeException(
-                        s"Unsupported return type ${item.getClass.toString}, ujson.Arr return type only supports [Double, Int, Boolean or String]"
+                      new ResponseMsg(
+                        msgId,
+                        upack.Null,
+                        upack.Float64(i.num)
                       )
                   }
-                  new ResponseMsg(
-                    msgId,
-                    upack.Null,
-                    args: _*
-                  )
+                  case b: ujson.Bool =>
+                    new ResponseMsg(
+                      msgId,
+                      upack.Null,
+                      upack.Bool(b.value)
+                    )
+                  case a: ujson.Arr => {
+                    import Conversions._
+                    var args = ArrayBuffer[upack.Msg]()
+                    for (item <- a.arr.map(_.value).toArray) {
+                      if (item.isInstanceOf[Double])
+                        args += (item.asInstanceOf[Double])
+                      else if (item.isInstanceOf[Int])
+                        args += (item.asInstanceOf[Int])
+                      else if (item.isInstanceOf[Boolean])
+                        args += (item.asInstanceOf[Boolean])
+                      else if (item.isInstanceOf[String])
+                        args += (item.toString)
+                      else
+                        throw new RuntimeException(
+                          s"Unsupported return type ${item.getClass.toString}, ujson.Arr return type only supports [Double, Int, Boolean, String, Unit]"
+                        )
+                    }
+                    new ResponseMsg(
+                      msgId,
+                      upack.Null,
+                      args: _*
+                    )
+                  }
+                  case _ =>
+                    val err = s"Unsupported return type: ${res.getClass.toString}"
+                    logger.error(err)
+                    throw new RuntimeException(err)
                 }
-                case _ =>
-                  val err = s"Unsupported return type: ${res.getClass.toString}"
-                  logger.error(err)
-                  throw new RuntimeException(err)
+                connection ! Write(ByteString(response.encode))
               }
-              connection ! Write(ByteString(response.encode))
             } catch {
               case e: Exception => {
-                val err = e.toString
-                logger.error(err)
+                logger.error(e.toString)
                 connection ! Write(
-                  ByteString(new ResponseMsg(msgId, upack.Str(err)).encode)
+                  ByteString(new ResponseMsg(msgId, upack.Str(e.getMessage)).encode)
                 )
               }
             }
